fc29c3c8511544152586183841d212abf40eac88
Luca De Dominicis <ddluca00@gmail.com>
2024-03-12 14:09:10 +0100

    Added sh files for training
    The sh files containes the right instruction to start the train for linemod or linemod-occluded.
    
    Signed-off-by: Luca De Dominicis <luca.dedominicis2@studio.unibo.it>

---
+++ train_lm.sh
@@ -0,0 +1,1 @@
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lm/a6_cPnP_lm13.py 0
---
+++ train_lmo.sh
@@ -0,0 +1,1 @@
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lmo/a6_cPnP_AugAAETrunc_BG0.5_lmo_real_pbr0.1_40e.py 0
