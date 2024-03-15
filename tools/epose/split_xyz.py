6f6ca8c9640f339b33a8eb78146ce706f725e359
Luca De Dominicis <ddluca00@gmail.com>
2024-03-15 15:00:13 +0100

    Fixed some errors and added some prints to understand what's going on
    Signed-off-by: Luca De Dominicis <luca.dedominicis2@studio.unibo.it>

--- tools/epose/split_xyz.py
+++ tools/epose/split_xyz.py
@@ -15,7 +15,9 @@ crop_path = Path(args.path + "/train_pbr" + "/xyz_crop" + "/000000/")
 # We define a set with all the image numbers
 flat_image_counts = set()
 
+print("Collecting image numbers...")
 for file in crop_path.glob("*"):
+    print(file.name)
     flat_image_counts.add(file.name.split("_")[0])
 
 flat_list = sorted(list(flat_image_counts))
@@ -26,6 +28,7 @@ how_many["2"] = {}
 how_many["3"] = {}
 how_many["4"] = {}
 
+print("Counting instances of each image...")
 for i in range(1,5):
     folder = dataset_path / f"00000{i}" / "mask"
     for file in folder.glob("*"):
@@ -36,9 +39,10 @@ for i in range(1,5):
 # Based on how_many we need to copy all instances of each image to the corresponding folder
 destination_root = dataset_path / "xyz_crop"
 
+print("Copying files...")
 # Create destination folders if they don't exist
 for i in range(1, 5):
-    (destination_root / f"folder_{i}").mkdir(parents=True, exist_ok=True)
+    (destination_root / f"{str(i).zfill(6)}").mkdir(parents=True, exist_ok=True)
 
 # Function to copy files to the corresponding folder based on how_many
 def copy_files_based_on_count():
@@ -46,10 +50,10 @@ def copy_files_based_on_count():
     for image in flat_list:
         arrived[image] = 0
     for i in range(1, 5):  # For each of the 4 folders
-        folder = f"folder_{i}"
+        folder = f"{str(i).zfill(6)}"
         for image_number, count in how_many[str(i)].items():
             for j in range(count):
-                shutil.copy(crop_path / f"{image_number}_{str(arrived[image_number]).zfill(6)}-xyz.pkl", destination_root / folder / f"{image_number}_{str(count).zfill(6)}-xyz.pkl")
+                shutil.copy(crop_path / f"{image_number}_{str(arrived[image_number]).zfill(6)}-xyz.pkl", destination_root / folder / f"{image_number}_{str(j).zfill(6)}-xyz.pkl")
                 arrived[image_number] = arrived.get(image_number, 0) + 1
 
 
