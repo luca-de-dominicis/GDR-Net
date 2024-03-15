9251725ee8118a71561b5428b68adb7b1ea0b173
Luca De Dominicis <ddluca00@gmail.com>
2024-03-15 14:57:15 +0100

    Updated training script for epose
    The script now takes care, if not done, of the dataset preparation before starting the train.
    
    Signed-off-by: Luca De Dominicis <luca.dedominicis2@studio.unibo.it>

--- train_epose.sh
+++ train_epose.sh
@@ -1,1 +1,25 @@
+#!/usr/bin/env bash
+# Ask the user if they prepared the dataset
+while true; do
+    read -p "Have you prepared the dataset? [y/n]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) 
+            echo "Ok it may take some time, I will ask you the necessary informations"
+            read -p "Please provide the dataset path: " userInput
+            echo "You entered: $userInput"
+            echo "1/4"
+            sh core/csrc/compile.sh
+            python tools/epose/compute_fps_epose.py
+            echo "2/4"
+            python tools/epose/generate_image_set.py --path $userInput
+            echo "3/4"
+            python tools/epose/generate_xyz_from_pbr.py
+            echo "4/4"
+            python tools/epose/split_xyz.py --path $userInput
+            echo "Done preparing the dataset"
+            break;; 
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 ./core/gdrn_modeling/train_gdrn.sh configs/gdrn/epose/config_epose.py 0
