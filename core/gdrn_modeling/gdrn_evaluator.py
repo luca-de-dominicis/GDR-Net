fc35c3c6ecfad4bb032489d432d55f504dcff3be
Luca De Dominicis <ddluca00@gmail.com>
2024-03-26 14:13:45 +0100

    Modified code to perform evaluation.
    The new code allow the user to perform evaluation on images with more than one instance per object.
    
    Signed-off-by: Luca De Dominicis <luca.dedominicis2@studio.unibo.it>

--- core/gdrn_modeling/gdrn_evaluator.py
+++ core/gdrn_modeling/gdrn_evaluator.py
@@ -10,7 +10,8 @@ import os.path as osp
 import random
 import time
 from collections import OrderedDict
-
+import pickle
+import json
 import cv2
 import mmcv
 import numpy as np
@@ -513,6 +514,7 @@ class GDRN_Evaluator(DatasetEvaluator):
         return results
 
 
+
 def gdrn_inference_on_dataset(cfg, model, data_loader, evaluator, amp_test=False):
     """Run model on the data_loader and evaluate the metrics with evaluator.
     Also benchmark the inference speed of `model.forward` accurately. The model
@@ -629,6 +631,125 @@ def gdrn_inference_on_dataset(cfg, model, data_loader, evaluator, amp_test=False
     if results is None:
         results = {}
     return results
+
+
+def gdrn_inference(cfg, model, data_loader, amp_test=False):
+    """Run model on the data_loader and evaluate the metrics with evaluator.
+    Also benchmark the inference speed of `model.forward` accurately. The model
+    will be used in eval mode.
+
+    Args:
+        model (nn.Module): a module which accepts an object from
+            `data_loader` and returns some outputs. It will be temporarily set to `eval` mode.
+
+            If you wish to evaluate a model in `training` mode instead, you can
+            wrap the given model and override its behavior of `.eval()` and `.train()`.
+        data_loader: an iterable object with a length.
+            The elements it generates will be the inputs to the model.
+        evaluator (DatasetEvaluator): the evaluator to run. Use `None` if you only want
+            to benchmark, but don't want to do any evaluation.
+
+    Returns:
+        The return value of `evaluator.evaluate()`
+    """
+    num_devices = get_world_size()
+    logger = logging.getLogger(__name__)
+    logger.info("Start inference on {} images".format(len(data_loader)))
+
+    total = len(data_loader)  # inference data loader must have a fixed length
+
+    num_warmup = min(5, total - 1)
+    start_time = time.perf_counter()
+    total_compute_time = 0
+    total_process_time = 0
+    with inference_context(model), torch.no_grad():
+        out = {}
+        for idx, inputs in enumerate(data_loader):
+            if idx == num_warmup:
+                start_time = time.perf_counter()
+                total_compute_time = 0
+                total_process_time = 0
+
+            start_compute_time = time.perf_counter()
+            #############################
+            # process input
+            if not isinstance(inputs, list):  # bs=1
+                inputs = [inputs]
+            batch = batch_data(cfg, inputs, phase="test")
+            if out.get(inputs[0]['file_name'][0].split('/')[-1], None) is None:
+                out[inputs[0]['file_name'][0].split('/')[-1]] = []
+
+            with autocast(enabled=amp_test):
+                out_dict = model(
+                    batch["roi_img"],
+                    roi_classes=batch["roi_cls"],
+                    roi_cams=batch["roi_cam"],
+                    roi_whs=batch["roi_wh"],
+                    roi_centers=batch["roi_center"],
+                    resize_ratios=batch["resize_ratio"],
+                    roi_coord_2d=batch.get("roi_coord_2d", None),
+                    roi_extents=batch.get("roi_extent", None),
+                )
+            if torch.cuda.is_available():
+                torch.cuda.synchronize()
+            cur_compute_time = time.perf_counter() - start_compute_time
+            total_compute_time += cur_compute_time
+            # NOTE: added
+            # TODO: add detection time here
+            outputs = [{} for _ in range(len(inputs))]
+            for _i in range(len(outputs)):
+                outputs[_i]["time"] = cur_compute_time
+
+            start_process_time = time.perf_counter()
+            #############################
+            # process output
+            for i in range(len(out_dict['trans'])):
+                ele = {}
+                ele['class'] = batch['roi_cls'][i].item()
+                ele['trans'] = out_dict['trans'][i].cpu().numpy().tolist()
+                ele['rot'] = out_dict['rot'][i].cpu().numpy().tolist()
+                out[inputs[0]['file_name'][0].split('/')[-1]].append(ele)
+            
+            # Save the results as pkl
+            with open("result.json", "w") as f:
+                json.dump(out, f)
+
+            cur_process_time = time.perf_counter() - start_process_time
+            total_process_time += cur_process_time
+
+            iters_after_start = idx + 1 - num_warmup * int(idx >= num_warmup)
+            seconds_per_img = total_compute_time / iters_after_start
+            if idx >= num_warmup * 2 or seconds_per_img > 5:
+                total_seconds_per_img = (time.perf_counter() - start_time) / iters_after_start
+                eta = datetime.timedelta(seconds=int(total_seconds_per_img * (total - idx - 1)))
+                log_every_n_seconds(
+                    logging.INFO, f"Inference done {idx+1}/{total}. {seconds_per_img:.4f} s / img. ETA={str(eta)}", n=5
+                )
+
+    # Measure the time only for this worker (before the synchronization barrier)
+    total_time = time.perf_counter() - start_time
+    total_time_str = str(datetime.timedelta(seconds=total_time))
+    # NOTE this format is parsed by grep
+    logger.info(
+        f"Total inference time: {total_time_str} "
+        f"({total_time / (total - num_warmup):.6f} s / img per device, on {num_devices} devices)"
+    )
+    # pure forward time
+    total_compute_time_str = str(datetime.timedelta(seconds=int(total_compute_time)))
+    logger.info(
+        "Total inference pure compute time: {} ({:.6f} s / img per device, on {} devices)".format(
+            total_compute_time_str, total_compute_time / (total - num_warmup), num_devices
+        )
+    )
+    # post_process time
+    total_process_time_str = str(datetime.timedelta(seconds=int(total_process_time)))
+    logger.info(
+        "Total inference post process time: {} ({:.6f} s / img per device, on {} devices)".format(
+            total_process_time_str, total_process_time / (total - num_warmup), num_devices
+        )
+    )
+
+    
 
 
 def save_result_of_dataset(cfg, model, data_loader, output_dir, dataset_name):
