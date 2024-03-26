dcbb2f9070e453c9c884add30e1d292aeade3fff
Luca De Dominicis <ddluca00@gmail.com>
2024-03-26 14:23:49 +0100

    Added better comments
    Signed-off-by: Luca De Dominicis <luca.dedominicis2@studio.unibo.it>

--- train_epose.sh
+++ train_epose.sh
@@ -8,18 +8,18 @@ while true; do
             echo "Ok it may take some time, I will ask you the necessary informations"
             read -p "Please provide the dataset path: " userInput
             echo "You entered: $userInput"
-            echo "1/4"
+            echo "1/4 compute fps"
             sh core/csrc/compile.sh
             python tools/epose/compute_fps_epose.py
-            echo "2/4"
+            echo "2/4 generate image set"
             python tools/epose/generate_image_set.py --path $userInput
-            echo "3/4"
+            echo "3/4 generate xyz"
             python tools/epose/generate_xyz_from_pbr.py
-            echo "4/4"
+            echo "4/4 split xyz"
             python tools/epose/split_xyz.py --path $userInput
             echo "Done preparing the dataset"
             break;; 
         * ) echo "Please answer yes or no.";;
     esac
 done
-./core/gdrn_modeling/train_gdrn.sh configs/gdrn/epose/config_epose.py 0
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/epose/config_epose_48_s0_sym_r32.py 0
