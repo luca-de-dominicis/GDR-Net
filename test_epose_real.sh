f6525f689f388469d4f4e2e4784592de8f25f81a
Luca De Dominicis <ddluca00@gmail.com>
2024-03-26 14:28:04 +0100

    Added script to inference on epose.
    Signed-off-by: Luca De Dominicis <luca.dedominicis2@studio.unibo.it>

---
+++ test_epose_real.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+./core/gdrn_modeling/test_gdrn.sh configs/gdrn/epose/config_epose_48_s0_sym_r32_realimg.py 0 /data/repos/GDR-Net/output/gdrn/epose/config_epose_48_s0_sym_r32/model_final.pth
