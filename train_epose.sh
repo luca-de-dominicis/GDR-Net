e9d445c007779b8e6797db6e3f6a16585235c9aa
Luca De Dominicis <ddluca00@gmail.com>
2024-04-17 11:37:32 +0200

    Minor fixes.
    Signed-off-by: Luca De Dominicis <ddluca00@gmail.com>

--- train_epose.sh
+++ train_epose.sh
@@ -10,11 +10,11 @@ while true; do
             echo "You entered: $userInput"
             echo "1/4 compute fps"
             sh core/csrc/compile.sh
-            python tools/epose/compute_fps_epose.py
+            python tools/epose/compute_fps_epose.py --path $userInput
             echo "2/4 generate image set"
             python tools/epose/generate_image_set.py --path $userInput
             echo "3/4 generate xyz"
-            python tools/epose/generate_xyz_from_pbr.py
+            python tools/epose/generate_xyz_from_pbr.py --path $userInput
             echo "4/4 split xyz"
             python tools/epose/split_xyz.py --path $userInput
             echo "Done preparing the dataset"
@@ -22,4 +22,4 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
-./core/gdrn_modeling/train_gdrn.sh configs/gdrn/epose/config_epose_48_s0_sym_r32.py 0
+./core/gdrn_modeling/train_gdrn.sh /data/repos/GDR-Net/configs/gdrn/epose/config_epose_48_s0_sym_r32_var_cam_standardsegmented_noshift.py 0
